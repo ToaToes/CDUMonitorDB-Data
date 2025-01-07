958319568d2c4719b23daeeb5fab4e075185ec91
Tom Chen <33406671+ToaToes@users.noreply.github.com>
2025-01-07 16:07:26 -0600

    Update CDU_API_WAlert.py

--- py_Scripts/CDU_API_WAlert.py
+++ py_Scripts/CDU_API_WAlert.py
@@ -1,3 +1,6 @@
+# Every (hour) generate result to one Excel report
+# Send Alert for exceeding threshold to WhatsAPP for Alert
+# No need for SSH or Password authentication
 # Author: Tom Chen
 
 import requests
